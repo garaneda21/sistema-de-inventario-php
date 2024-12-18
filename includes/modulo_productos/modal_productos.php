01039cbbd00423077ce690b2936f41095e39e0eb
garaneda21 <garaneda@utem.cl>
2024-12-18 00:11:09 -0300

    Eliminado categoria de productos

--- includes/modulo_productos/modal_productos.php
+++ includes/modulo_productos/modal_productos.php
@@ -6,9 +6,7 @@ require_once "includes/db.php";
 require_once "includes/modulo_productos/modal_productos_modelo.php";
 require_once "includes/modulo_productos/modal_productos_vista.php";
 
-$categorias = obtener_categorias($pdo);
 $unidades_de_medida = obtener_unidad($pdo);
-
 
 // Cerrar conexion con base de datos (hacer siempre);
 $pdo = null;
--- includes/modulo_productos/modal_productos_modelo.php
+++ includes/modulo_productos/modal_productos_modelo.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-function obtener_categorias(object $pdo) {
-    $consulta = "SELECT * FROM categoria";
-
-    $stmt = $pdo->prepare($consulta);
-    $stmt->execute();
-    $resul = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $resul;
-}
-
 function obtener_unidad(object $pdo) {
     $consulta = "SELECT * FROM unidad_de_medida";
 
